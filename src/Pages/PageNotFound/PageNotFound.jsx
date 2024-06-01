a61a18bd22a50393717a3d0fb0707a040af0b7fd
Miguel <miguelmpp12@gmail.com>
2024-06-01 20:01:30 -0300

    estilizando page not found

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import './PageNotFound.scss'; // Importe o arquivo SCSS
+import './PageNotFound.scss'; // Importe o arquivo SCSS
 
 const PageNotFound = () => {
   return (
     <div className="page-not-found">
-      <div className="content">
-        <h1>404</h1>
-        <h2>Oops! Página não encontrada</h2>
-        <p>A página que você está procurando pode ter sido removida, teve seu nome alterado ou está temporariamente indisponível.</p>
-        <Link to="/" className="home-link">Voltar para a Página Inicial</Link>
+      <div className="content-overlay">
+        <div className="content">
+          <h1>404</h1>
+          <h2>Oops! Página não encontrada</h2>
+          <p>A página que você está procurando pode ter sido removida, teve seu nome alterado ou está temporariamente indisponível.</p>
+          <Link to="/" className="home-link">Voltar para a Página Inicial</Link>
+        </div>
       </div>
     </div>
   );
